6236917c0ad8e11a34fe3851a4fb6a043efde23d
Kalin Ivanov <kalin2578@gmail.com>
2023-11-26 20:37:46 +0200

    tweak minor change - added constant for example value

--- java/src/main/java/com/java/stuff/example/SmartIndentation.java
+++ java/src/main/java/com/java/stuff/example/SmartIndentation.java
@@ -5,12 +5,14 @@ import org.slf4j.LoggerFactory;
 
 public class SmartIndentation {
   private static final Logger LOGGER = LoggerFactory.getLogger(SmartIndentation.class);
-  public static String smartIndentation() {
-    return """
+  private static final String MESSAGE = """
             It is great
             when compilers care about conventions
             Makes our life easier
             """;
+
+  public static String smartIndentation() {
+    return MESSAGE;
   }
 
   public static void main(String[] args) {
