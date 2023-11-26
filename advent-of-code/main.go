946ae77dbc786b6f1072276d20722b3139b7d322
Kalin Ivanov <kalin2578@gmail.com>
2023-11-26 10:03:15 +0200

    simple test in Go - app as a server

--- advent-of-code/main.go
+++ advent-of-code/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+	r.GET("/hello", hello)
+	log.Fatal(r.Run(":8080"))
+}
+
+func hello(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello World",
+	})
 }
