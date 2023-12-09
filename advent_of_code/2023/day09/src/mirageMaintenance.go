9404c15c56dd0d16202d22cdead36a14d309b757
Kalin Ivanov <kalin2578@gmail.com>
2023-12-09 12:26:40 +0200

    aoC day09 - mirageMaintenance first part

---
+++ advent_of_code/2023/day09/src/mirageMaintenance.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, _ := os.Open("2023/day09/src/input.txt")
+	defer file.Close()
+
+	var inputHistories [][]int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		history, _ := parseLineToHistory(line)
+		inputHistories = append(inputHistories, history)
+	}
+
+	sum := 0
+	for _, history := range inputHistories {
+		nextValue := extrapolateNextValue(history)
+		sum += nextValue
+	}
+	fmt.Println("Sum of next values:", sum)
+}
+
+func parseLineToHistory(line string) ([]int, error) {
+	var history []int
+	values := strings.Split(line, " ")
+	for _, value := range values {
+		intValue, err := strconv.Atoi(value)
+		if err != nil {
+			return nil, err
+		}
+		history = append(history, intValue)
+	}
+	return history, nil
+}
+
+func extrapolateNextValue(history []int) int {
+	sequences := make([][]int, 0)
+	sequences = append(sequences, history)
+
+	for {
+		lastSeq := sequences[len(sequences)-1]
+		newSeq := make([]int, 0)
+		isZeroSeq := true
+
+		for i := 0; i < len(lastSeq)-1; i++ {
+			diff := lastSeq[i+1] - lastSeq[i]
+			newSeq = append(newSeq, diff)
+			if diff != 0 {
+				isZeroSeq = false
+			}
+		}
+		sequences = append(sequences, newSeq)
+		if isZeroSeq {
+			break
+		}
+	}
+	for i := len(sequences) - 2; i >= 0; i-- {
+		sequences[i] = append(sequences[i], sequences[i][len(sequences[i])-1]+sequences[i+1][len(sequences[i+1])-1])
+	}
+	return sequences[0][len(sequences[0])-1]
+}
