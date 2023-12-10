bfda9e8bdc2c44a3362fabea5d87e776ad411dca
Kalin Ivanov <kalin2578@gmail.com>
2023-12-10 23:13:57 +0200

    tweak some experiments

---
+++ java/src/main/java/com/java/features/examples/CoinFlipProbability.java
@@ -0,0 +1,33 @@
+package com.java.features.examples;
+
+import java.math.BigInteger;
+import java.util.stream.IntStream;
+
+public class CoinFlipProbability {
+
+  private CoinFlipProbability() throws InstantiationException {
+    throw new InstantiationException("Should not create an instance of this class");
+  }
+
+  public static BigInteger calculateProbabilityNoHeads(int n) {
+    return BigInteger.ONE.divide(BigInteger.valueOf(2).pow(n));
+  }
+
+  public static BigInteger calculateProbabilityFewerThanNHeads(int n) {
+    return IntStream.rangeClosed(0, n - 1)
+            .mapToObj(k -> binomialCoefficient(4 * n, k)
+                    .shiftLeft(4 * n)
+                    .modInverse(BigInteger.ONE.shiftLeft(4 * n)))
+            .reduce(BigInteger.ZERO, BigInteger::add);
+  }
+
+  private static BigInteger binomialCoefficient(int n, int k) {
+    return factorial(n).divide(factorial(k).multiply(factorial(n - k)));
+  }
+
+  private static BigInteger factorial(int n) {
+    return IntStream.rangeClosed(2, n)
+            .mapToObj(BigInteger::valueOf)
+            .reduce(BigInteger.ONE, BigInteger::multiply);
+  }
+}
