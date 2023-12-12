9ea8a69f48c9d1f10bdfb9050352f58dc203b841
Kalin Ivanov <kalin2578@gmail.com>
2023-12-12 08:32:06 +0200

    part one

---
+++ advent_of_code/2023/day12/src/hotSprings.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type key struct {
+	lava string
+	nums string
+	has  bool
+}
+
+var cache map[key]int
+
+func sliceToString(slice []int) string {
+	return strings.Trim(strings.Replace(fmt.Sprint(slice), " ", ",", -1), "[]")
+}
+
+func solve(lava string, nums []int, has bool) int {
+	k := key{lava, sliceToString(nums), has}
+	if val, found := cache[k]; found {
+		return val
+	}
+	if len(lava) == 0 {
+		if len(nums) == 0 || (len(nums) == 1 && nums[0] == 0) {
+			return 1
+		}
+		return 0
+	}
+
+	var result int
+	switch {
+	case lava[0] == '#' && len(nums) > 0 && nums[0] > 0:
+		numsCopy := append([]int{nums[0] - 1}, nums[1:]...)
+		result = solve(lava[1:], numsCopy, true)
+	case lava[0] == '.' && has && len(nums) > 0 && nums[0] == 0:
+		result = solve(lava[1:], nums[1:], false)
+	case lava[0] == '.' && !has:
+		result = solve(lava[1:], nums, false)
+	case lava[0] == '?':
+		result = solve("#"+lava[1:], nums, has) + solve("."+lava[1:], nums, has)
+	}
+	cache[k] = result
+	return result
+}
+
+func main() {
+	cache = make(map[key]int)
+	file, _ := os.Open("2023/day12/src/input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	total := 0
+	for scanner.Scan() {
+		parts := strings.Split(scanner.Text(), " ")
+		lava := parts[0]
+		numsStr := strings.Split(parts[1], ",")
+		var nums []int
+		for _, s := range numsStr {
+			n, _ := strconv.Atoi(s)
+			nums = append(nums, n)
+		}
+		total += solve(lava, nums, false)
+	}
+	fmt.Println("Total number of arrangements:", total)
+}
