b12697a739a9fadf23db58632937d37b620cff65
Kalin Ivanov <kalin2578@gmail.com>
2023-12-14 19:21:56 +0200

    aoC day14 - Parabolic Reflector Dish

--- advent_of_code/2023/day14/src/parabolicReflectorDish.go
+++ advent_of_code/2023/day14/src/parabolicReflectorDish.go
@@ -4,58 +4,144 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+)
+
+type Point struct {
+	X, Y int
+}
+
+func (p Point) Add(direction Point) Point {
+	return Point{X: p.X + direction.X, Y: p.Y + direction.Y}
+}
+
+var (
+	North = Point{0, -1}
+	East  = Point{1, 0}
+	South = Point{0, 1}
+	West  = Point{-1, 0}
 )
 
 func main() {
 	grid, _ := readInput("2023/day14/src/input.txt")
-	tiltNorth(grid)
+	grid = runCycles(grid, 1_000_000_000)
 	totalLoad := calculateTotalLoad(grid)
 	fmt.Println("Total Load:", totalLoad)
 }
 
-func readInput(filePath string) ([][]rune, error) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
-	}
+func readInput(filePath string) (map[Point]rune, error) {
+	file, _ := os.Open(filePath)
 	defer file.Close()
 
-	var grid [][]rune
+	grid := make(map[Point]rune)
 	scanner := bufio.NewScanner(file)
+	y := 0
 	for scanner.Scan() {
-		grid = append(grid, []rune(scanner.Text()))
+		for x, c := range scanner.Text() {
+			grid[Point{X: x, Y: y}] = c
+		}
+		y++
 	}
 	return grid, scanner.Err()
 }
 
-func tiltNorth(grid [][]rune) {
-	for y := 0; y < len(grid); y++ {
-		for x := 0; x < len(grid[0]); x++ {
-			if grid[y][x] == 'O' {
-				newY := y
-				for k := y - 1; k >= 0; k-- {
-					if grid[k][x] == '.' {
-						newY = k
-					} else {
-						break
-					}
+func tilt(grid map[Point]rune, dir Point) {
+	var points []Point
+	for p := range grid {
+		points = append(points, p)
+	}
+
+	sort.Slice(points, func(i, j int) bool {
+		if dir.Y != 0 {
+			return points[i].Y*dir.Y > points[j].Y*dir.Y
+		}
+		return points[i].X*dir.X > points[j].X*dir.X
+	})
+
+	for _, p := range points {
+		if grid[p] == 'O' {
+			nextP := p
+			for {
+				nextP = nextP.Add(dir)
+				if grid[nextP] != '.' {
+					break
 				}
-				if newY != y {
-					grid[newY][x], grid[y][x] = 'O', '.'
-				}
+				grid[p], grid[nextP] = '.', 'O'
+				p = nextP
 			}
 		}
 	}
 }
 
-func calculateTotalLoad(grid [][]rune) int {
+func runCycles(grid map[Point]rune, n int) map[Point]rune {
+	seen := make(map[string]int)
+	var remainingCycles int
+
+	for cycle := 1; cycle <= n; cycle++ {
+		gridStr := gridToString(grid)
+		if firstSeenIndex, exists := seen[gridStr]; exists {
+			cycleLength := cycle - firstSeenIndex
+			remainingCycles = (n - cycle) % cycleLength
+			break
+		} else {
+			seen[gridStr] = cycle
+			tilt(grid, North)
+			tilt(grid, West)
+			tilt(grid, South)
+			tilt(grid, East)
+		}
+	}
+	for i := 0; i < remainingCycles; i++ {
+		tilt(grid, North)
+		tilt(grid, West)
+		tilt(grid, South)
+		tilt(grid, East)
+	}
+	return grid
+}
+
+func calculateTotalLoad(grid map[Point]rune) int {
 	totalLoad := 0
-	for y := 0; y < len(grid); y++ {
-		for x := 0; x < len(grid[0]); x++ {
-			if grid[y][x] == 'O' {
-				totalLoad += len(grid) - y
-			}
+	maxY := findMaxY(grid)
+	for p, v := range grid {
+		if v == 'O' {
+			totalLoad += maxY - p.Y + 1
 		}
 	}
 	return totalLoad
+}
+
+func findMaxY(grid map[Point]rune) int {
+	maxY := 0
+	for p := range grid {
+		if p.Y > maxY {
+			maxY = p.Y
+		}
+	}
+	return maxY
+}
+
+func gridToString(grid map[Point]rune) string {
+	maxX, maxY := findMaxXY(grid)
+	var result string
+	for y := 0; y <= maxY; y++ {
+		for x := 0; x <= maxX; x++ {
+			result += string(grid[Point{X: x, Y: y}])
+		}
+		result += "\n"
+	}
+	return result
+}
+
+func findMaxXY(grid map[Point]rune) (int, int) {
+	maxX, maxY := 0, 0
+	for p := range grid {
+		if p.X > maxX {
+			maxX = p.X
+		}
+		if p.Y > maxY {
+			maxY = p.Y
+		}
+	}
+	return maxX, maxY
 }
