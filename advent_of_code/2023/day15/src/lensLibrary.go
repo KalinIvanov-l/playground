23b1221381d97fa79c803f2406cc02543c89051f
Kalin Ivanov <kalin2578@gmail.com>
2023-12-15 08:51:15 +0200

    aoC day15 - Lens Library

--- advent_of_code/2023/day15/src/lensLibrary.go
+++ advent_of_code/2023/day15/src/lensLibrary.go
@@ -2,21 +2,48 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"io/ioutil"
 	"strings"
 )
 
+type Lens struct {
+	Label       string
+	FocalLength int
+}
+
 func main() {
-	data, err := os.ReadFile("2023/day15/src/input.txt")
+	data, err := ioutil.ReadFile("2023/day15/src/input.txt")
 	if err != nil {
 		panic(err)
 	}
 	steps := strings.Split(strings.TrimSpace(string(data)), ",")
-	sum := 0
+	boxes := make([][]Lens, 256) // Initialize boxes
+
 	for _, step := range steps {
-		sum += hash(step)
+		processStep(step, &boxes)
 	}
-	fmt.Println("Sum of HASH algorithm results:", sum)
+	totalFocusPower := 0
+	for boxIndex, box := range boxes {
+		for slotIndex, lens := range box {
+			totalFocusPower += (boxIndex + 1) * (slotIndex + 1) * lens.FocalLength
+		}
+	}
+	fmt.Println("Total focusing power:", totalFocusPower)
+}
+
+func processStep(step string, boxes *[][]Lens) {
+	label := step
+	if strings.Contains(step, "=") {
+		parts := strings.Split(step, "=")
+		label = parts[0]
+		focalLength := atomic(parts[1])
+		boxIndex := hash(label)
+		replaceOrInsertLens(label, focalLength, &(*boxes)[boxIndex])
+	} else if strings.Contains(step, "-") {
+		label = strings.TrimSuffix(label, "-")
+		boxIndex := hash(label)
+		removeLens(label, &(*boxes)[boxIndex])
+	}
 }
 
 func hash(s string) int {
@@ -25,4 +52,31 @@ func hash(s string) int {
 		currentValue = (currentValue + int(c)) * 17 % 256
 	}
 	return currentValue
+}
+
+func replaceOrInsertLens(label string, focalLength int, box *[]Lens) {
+	for i, lens := range *box {
+		if lens.Label == label {
+			(*box)[i].FocalLength = focalLength
+			return
+		}
+	}
+	*box = append(*box, Lens{Label: label, FocalLength: focalLength})
+}
+
+func removeLens(label string, box *[]Lens) {
+	for i, lens := range *box {
+		if lens.Label == label {
+			*box = append((*box)[:i], (*box)[i+1:]...)
+			return
+		}
+	}
+}
+
+func atomic(s string) int {
+	res := 0
+	for _, c := range s {
+		res = res*10 + int(c-'0')
+	}
+	return res
 }
