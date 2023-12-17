22ea95efa108b51c129ea09943feeeff0caabcc1
Kalin Ivanov <kalin2578@gmail.com>
2023-12-17 15:40:37 +0200

    test4

--- advent_of_code/2023/day17/src/clumsyCrucible.go
+++ advent_of_code/2023/day17/src/clumsyCrucible.go
@@ -8,13 +8,13 @@ import (
 	"strings"
 )
 
+// State represents the current state in the pathfinding process.
 type State struct {
-	pos   complex128
-	dir   complex128
-	loss  int
-	index int
+	loss, chain, direction int
+	pos                    complex128
 }
 
+// PriorityQueue implements heap.Interface and holds States.
 type PriorityQueue []*State
 
 func (pq PriorityQueue) Len() int { return len(pq) }
@@ -25,34 +25,29 @@ func (pq PriorityQueue) Less(i, j int) bool {
 
 func (pq PriorityQueue) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
 }
 
 func (pq *PriorityQueue) Push(x interface{}) {
-	n := len(*pq)
-	item := x.(*State)
-	item.index = n
-	*pq = append(*pq, item)
+	*pq = append(*pq, x.(*State))
 }
 
 func (pq *PriorityQueue) Pop() interface{} {
 	old := *pq
 	n := len(old)
 	item := old[n-1]
-	old[n-1] = nil
-	item.index = -1
 	*pq = old[0 : n-1]
 	return item
 }
 
-func readInput(filename string) map[complex128]int {
+// readGrid reads the grid from a file and returns a map with the heat loss for each coordinate.
+func readGrid(filename string) (map[complex128]int, complex128) {
 	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
 	lines := strings.Split(string(bytes), "\n")
 	grid := make(map[complex128]int)
+	var maxX, maxY int
 	for y, line := range lines {
 		for x, char := range line {
 			value, err := strconv.Atoi(string(char))
@@ -60,48 +55,62 @@ func readInput(filename string) map[complex128]int {
 				panic(err)
 			}
 			grid[complex(float64(x), float64(y))] = value
+			if x > maxX {
+				maxX = x
+			}
 		}
+		maxY = y
 	}
-	return grid
+	end := complex(float64(maxX), float64(maxY))
+	return grid, end
 }
 
-func main() {
-	grid := readInput("2023/day17/src/input.txt")
-	target := complex128(0)
-	for pos := range grid {
-		if real(pos) > real(target) || imag(pos) > imag(target) {
-			target = pos
-		}
-	}
-
-	pq := make(PriorityQueue, 0)
-	heap.Init(&pq)
-	heap.Push(&pq, &State{0, 1, 0, 0})
-	heap.Push(&pq, &State{0, 1i, 0, 0})
-
-	visited := make(map[complex128]map[complex128]bool)
+// findPath implements the pathfinding logic using a priority queue.
+func findPath(grid map[complex128]int, start, end complex128, a, b, c int) int {
+	pq := &PriorityQueue{}
+	heap.Init(pq)
+	heap.Push(pq, &State{0, 1, 0, start})
+	best := make(map[complex128]map[int]map[int]int)
 
 	for pq.Len() > 0 {
-		state := heap.Pop(&pq).(*State)
-		if state.pos == target {
-			fmt.Println("Minimum heat loss:", state.loss)
-			return
+		current := heap.Pop(pq).(*State)
+		if current.pos == end && current.chain >= a {
+			return current.loss
 		}
-		if _, ok := visited[state.pos]; !ok {
-			visited[state.pos] = make(map[complex128]bool)
+
+		if _, ok := best[current.pos]; !ok {
+			best[current.pos] = make(map[int]map[int]int)
 		}
-		if _, visited := visited[state.pos][state.dir]; visited {
+		if _, ok := best[current.pos][current.direction]; !ok {
+			best[current.pos][current.direction] = make(map[int]int)
+		}
+		if val, ok := best[current.pos][current.direction][current.chain]; ok && val <= current.loss {
 			continue
 		}
-		visited[state.pos][state.dir] = true
+		best[current.pos][current.direction][current.chain] = current.loss
 
-		for _, dirChange := range []complex128{1, -1} {
-			newDir := state.dir * dirChange
-			newPos := state.pos + newDir
-			if _, exists := grid[newPos]; exists {
-				newLoss := state.loss + grid[newPos]
-				heap.Push(&pq, &State{newPos, newDir, newLoss, 0})
+		directions := []complex128{1, 1i, -1, -1i}
+		for e, d := range directions {
+			nextChain := 1
+			if e%2 == 0 {
+				nextChain = current.chain + 1
+			}
+			if (e%2 == 0 && current.chain < b) || (e%2 != 0 && current.chain == c) {
+				continue
+			}
+			neighbour := current.pos + d
+			if loss, ok := grid[neighbour]; ok {
+				heap.Push(pq, &State{current.loss + loss, nextChain, e, neighbour})
 			}
 		}
 	}
+
+	return -1
+}
+
+func main() {
+	grid, end := readGrid("2023/day17/src/input.txt")
+	start := complex(0, 0)
+	fmt.Println("Minimum heat loss:", findPath(grid, start, end, 0, 0, 3))
+	fmt.Println("Minimum heat loss:", findPath(grid, start, end, 4, 4, 10))
 }
