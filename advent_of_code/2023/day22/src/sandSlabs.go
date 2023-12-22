b3b3e214fe5f50da38859e78a622b8e56899673e
Kalin Ivanov <kalin2578@gmail.com>
2023-12-22 08:06:22 +0200

    aoC day22 - Sand Slabs

--- advent_of_code/2023/day22/src/sandSlabs.go
+++ advent_of_code/2023/day22/src/sandSlabs.go
@@ -4,67 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 )
 
-type Brick struct {
-	x1, y1, z1, x2, y2, z2 int
+type Coord struct {
+	x, y, z int
 }
 
-func NewBrick(coords []int) Brick {
-	return Brick{
-		x1: coords[0], y1: coords[1], z1: coords[2],
-		x2: coords[3], y2: coords[4], z2: coords[5],
-	}
-}
-
-func droppedBrick(tallest map[[2]int]int, brick Brick) Brick {
-	peak := 0
-	for x := brick.x1; x <= brick.x2; x++ {
-		for y := brick.y1; y <= brick.y2; y++ {
-			if tallest[[2]int{x, y}] > peak {
-				peak = tallest[[2]int{x, y}]
-			}
-		}
-	}
-	dz := max(brick.z1-peak-1, 0)
-	return Brick{brick.x1, brick.y1, brick.z1 - dz, brick.x2, brick.y2, brick.z2 - dz}
-}
-
-func drop(tower []Brick) (int, []Brick) {
-	tallest := make(map[[2]int]int)
-	newTower := make([]Brick, len(tower))
-	falls := 0
-	for i, brick := range tower {
-		newBrick := droppedBrick(tallest, brick)
-		if newBrick.z1 != brick.z1 {
-			falls++
-		}
-		newTower[i] = newBrick
-		for x := brick.x1; x <= brick.x2; x++ {
-			for y := brick.y1; y <= brick.y2; y++ {
-				tallest[[2]int{x, y}] = newBrick.z2
-			}
-		}
-	}
-	return falls, newTower
-}
-
-func solve(bricks []Brick) int {
-	_, fallen := drop(bricks)
-	p1 := 0
-	for i := range fallen {
-		removed := append([]Brick{}, fallen[:i]...)
-		removed = append(removed, fallen[i+1:]...)
-		falls, _ := drop(removed)
-		if falls == 0 {
-			p1++
-		}
-	}
-	return p1
-}
+type Brick []Coord
 
 func main() {
 	file, _ := os.Open("2023/day22/src/input.txt")
@@ -74,31 +22,151 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		coords := parseLine(line)
-		bricks = append(bricks, NewBrick(coords))
+		bricks = append(bricks, parseBrick(line))
 	}
-	sort.Slice(bricks, func(i, j int) bool {
-		return bricks[i].z1 < bricks[j].z1
-	})
 
-	p1 := solve(bricks)
-	fmt.Printf("Part 1: %d\n", p1)
+	seen := make(map[Coord]bool)
+	for _, brick := range bricks {
+		for _, coord := range brick {
+			seen[coord] = true
+		}
+	}
+	simulateBricksFalling(bricks, seen)
+	oldSeen := deepcopy(seen)
+	oldBricks := deepcopyBricks(bricks)
+
+	p1, p2 := 0, 0
+	for i, brick := range bricks {
+		seen = deepcopy(oldSeen)
+		bricks = deepcopyBricks(oldBricks)
+
+		for _, coord := range brick {
+			delete(seen, coord)
+		}
+
+		fall := make(map[int]struct{})
+		for {
+			a := false
+			for j, b := range bricks {
+				if j == i {
+					continue
+				}
+				ok := true
+				for _, c := range b {
+					if c.z == 1 || (seen[Coord{c.x, c.y, c.z - 1}] && !containsCoord(b, Coord{c.x, c.y, c.z - 1})) {
+						ok = false
+						break
+					}
+				}
+				if ok {
+					a = true
+					fall[j] = struct{}{}
+					for _, c := range b {
+						delete(seen, c)
+						seen[Coord{c.x, c.y, c.z - 1}] = true
+					}
+					bricks[j] = moveBrickDown(b)
+				}
+			}
+			if !a {
+				break
+			}
+		}
+		if len(fall) == 0 {
+			p1++
+		}
+		p2 += len(fall)
+	}
+	fmt.Println("Part 1:", p1)
+	fmt.Println("Part 2:", p2)
 }
 
-func parseLine(line string) []int {
+func parseBrick(line string) Brick {
 	parts := strings.Split(line, "~")
-	allCoords := strings.Split(parts[0]+","+parts[1], ",")
-	var coords []int
-	for _, str := range allCoords {
-		num, _ := strconv.Atoi(str)
-		coords = append(coords, num)
+	start := parseCoord(parts[0])
+	end := parseCoord(parts[1])
+
+	var brick Brick
+	if start.x == end.x && start.y == end.y {
+		for z := start.z; z <= end.z; z++ {
+			brick = append(brick, Coord{start.x, start.y, z})
+		}
+	} else if start.x == end.x && start.z == end.z {
+		for y := start.y; y <= end.y; y++ {
+			brick = append(brick, Coord{start.x, y, start.z})
+		}
+	} else if start.y == end.y && start.z == end.z {
+		for x := start.x; x <= end.x; x++ {
+			brick = append(brick, Coord{x, start.y, start.z})
+		}
 	}
-	return coords
+	return brick
 }
 
-func max(a, b int) int {
-	if a > b {
-		return a
+func parseCoord(part string) Coord {
+	values := strings.Split(part, ",")
+	x, _ := strconv.Atoi(values[0])
+	y, _ := strconv.Atoi(values[1])
+	z, _ := strconv.Atoi(values[2])
+	return Coord{x, y, z}
+}
+
+func simulateBricksFalling(bricks []Brick, seen map[Coord]bool) {
+	for {
+		a := false
+		for i := range bricks {
+			ok := true
+			for _, coord := range bricks[i] {
+				if coord.z == 1 || (seen[Coord{coord.x, coord.y, coord.z - 1}] && !containsCoord(bricks[i], Coord{coord.x, coord.y, coord.z - 1})) {
+					ok = false
+					break
+				}
+			}
+			if ok {
+				a = true
+				for _, coord := range bricks[i] {
+					delete(seen, coord)
+					seen[Coord{coord.x, coord.y, coord.z - 1}] = true
+				}
+				bricks[i] = moveBrickDown(bricks[i])
+			}
+		}
+		if !a {
+			break
+		}
 	}
-	return b
+}
+
+func deepcopy(m map[Coord]bool) map[Coord]bool {
+	cp := make(map[Coord]bool)
+	for k, v := range m {
+		cp[k] = v
+	}
+	return cp
+}
+
+func deepcopyBricks(bricks []Brick) []Brick {
+	cp := make([]Brick, len(bricks))
+	for i, b := range bricks {
+		cp[i] = make(Brick, len(b))
+		copy(cp[i], b)
+	}
+	return cp
+}
+
+func containsCoord(brick Brick, coord Coord) bool {
+	for _, c := range brick {
+		if c == coord {
+			return true
+		}
+	}
+	return false
+}
+
+func moveBrickDown(brick Brick) Brick {
+	moved := make(Brick, len(brick))
+	for i, coord := range brick {
+		moved[i] = Coord{coord.x, coord.y, coord.z - 1}
+	}
+	return moved
 }
