dc676a77cf640706efa34888560b6d7a396adc68
Kalin Ivanov <kalin2578@gmail.com>
2023-12-24 17:55:45 +0200

    aoC day24 - Never Tell Me The Odds

--- advent_of_code/2023/day24/src/odds.go
+++ advent_of_code/2023/day24/src/odds.go
@@ -10,68 +10,76 @@ import (
 
 const (
 	InputFile = "2023/day24/src/input.txt"
-	Min       = 200000000000000
-	Max       = 400000000000000
+	LEAST     = 200000000000000
+	MOST      = 400000000000000
 )
 
-type Point struct {
-	X, Y float64
+type Line struct {
+	a, b, c float64
 }
 
-func intersect(x1, y1, x2, y2, x3, y3, x4, y4 float64) *Point {
-	denom := (y4-y3)*(x2-x1) - (x4-x3)*(y2-y1)
-	ua := ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / denom
-	return &Point{X: x1 + ua*(x2-x1), Y: y1 + ua*(y2-y1)}
+type Stone struct {
+	x, y, z    int
+	vx, vy, vz int
+	line       Line
 }
 
-func main() {
+func parseNums(str string) (int, int, int) {
+	parts := strings.Split(str, ", ")
+	x, _ := strconv.Atoi(strings.TrimSpace(parts[0]))
+	y, _ := strconv.Atoi(strings.TrimSpace(parts[1]))
+	z, _ := strconv.Atoi(strings.TrimSpace(parts[2]))
+	return x, y, z
+}
+
+func Solve() {
 	file, _ := os.Open(InputFile)
 	defer file.Close()
 
-	var lines []string
+	var stones []Stone
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+		p, v, _ := strings.Cut(scanner.Text(), " @ ")
+		x, y, z := parseNums(p)
+		vx, vy, vz := parseNums(v)
+
+		m := float64(vy) / float64(vx)
+		c := float64(y) - m*float64(x)
+		stones = append(stones, Stone{x, y, z, vx, vy, vz, Line{m, -1, c}})
 	}
 
-	bean := 0
-	for i := 0; i < len(lines); i++ {
-		for j := i + 1; j < len(lines); j++ {
-			line1 := strings.Split(lines[i], " @ ")
-			line2 := strings.Split(lines[j], " @ ")
+	count := 0
+	for i := 0; i < len(stones)-1; i++ {
+		for j := i + 1; j < len(stones); j++ {
+			s1, s2 := stones[i], stones[j]
+			a1b2A2b1 := (s1.line.a * s2.line.b) - (s2.line.a * s1.line.b)
+			if a1b2A2b1 == 0 {
+				continue
+			}
 
-			pos1 := strings.Split(line1[0], ", ")
-			vel1 := strings.Split(line1[1], ", ")
-			pos2 := strings.Split(line2[0], ", ")
-			vel2 := strings.Split(line2[1], ", ")
+			ix, iy := (s1.line.b*s2.line.c-s2.line.b*s1.line.c)/a1b2A2b1, (s1.line.c*s2.line.a-s2.line.c*s1.line.a)/a1b2A2b1
+			t1, t2 := (ix-float64(s1.x))/float64(s1.vx), (ix-float64(s2.x))/float64(s2.vx)
 
-			x1, _ := strconv.ParseFloat(pos1[0], 64)
-			y1, _ := strconv.ParseFloat(pos1[1], 64)
-			x2 := x1 + parseFloat(vel1[0])
-			y2 := y1 + parseFloat(vel1[1])
-
-			x3, _ := strconv.ParseFloat(pos2[0], 64)
-			y3, _ := strconv.ParseFloat(pos2[1], 64)
-			x4 := x3 + parseFloat(vel2[0])
-			y4 := y3 + parseFloat(vel2[1])
-
-			intersection := intersect(x1, y1, x2, y2, x3, y3, x4, y4)
-			if intersection != nil {
-				x := intersection.X
-				y := intersection.Y
-
-				if (x > x1) == (x2 > x1) && (y > y1) == (y2 > y1) &&
-					(x > x3) == (x4 > x3) && (y > y3) == (y4 > y3) &&
-					x >= Min && x <= Max && y >= Min && y <= Max {
-					bean++
-				}
+			if t1 > 0 && t2 > 0 && ix >= LEAST && ix <= MOST && iy >= LEAST && iy <= MOST {
+				count++
 			}
 		}
 	}
-	fmt.Println("answer part 1:", bean)
+	fmt.Println("Part 1: ", count)
+
+	fmt.Println("var('x y z vx vy vz t1 t2 t3 ans')")
+	for i := 0; i < 3; i++ {
+		fmt.Printf("eq%d = x + (vx * t%d) == %d + (%d * t%d)\n", i*3+1, i+1, stones[i].x, stones[i].vx, i+1)
+		fmt.Printf("eq%d = y + (vy * t%d) == %d + (%d * t%d)\n", i*3+2, i+1, stones[i].y, stones[i].vy, i+1)
+		fmt.Printf("eq%d = z + (vz * t%d) == %d + (%d * t%d)\n", i*3+3, i+1, stones[i].z, stones[i].vz, i+1)
+	}
+	fmt.Println("eq10 = ans == x + y + z")
+	fmt.Println("print(solve([eq1,eq2,eq3,eq4,eq5,eq6,eq7,eq8,eq9,eq10],x,y,z,vx,vy,vz,t1,t2,t3,ans))")
+
+	fmt.Println()
+	fmt.Println("Part 2: ", 149412455352770+174964385672289+233413147425100)
 }
 
-func parseFloat(s string) float64 {
-	v, _ := strconv.ParseFloat(s, 64)
-	return v
+func main() {
+	Solve()
 }
