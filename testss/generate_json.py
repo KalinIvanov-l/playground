735fabe5c8dd1625504d0233ee4794bc5719966f
Kalin Ivanov <kalin2578@gmail.com>
2023-12-29 21:20:52 +0200

    initial simple script

---
+++ testss/generate_json.py
@@ -0,0 +1,18 @@
+import json
+
+sample_data = {
+    "example data..",
+    "....",
+    "...."
+}
+
+num_objects = 10000
+
+filename = "path/to/example_file.txt"
+
+with open(filename, 'w') as file :
+    for i in range(num_objects) :
+        modulefinder = sample_data.copy()
+        modulefinder("fileds") += str(i)
+        data_str = json.dumps(modulefinder)
+        # file.write(data_str + "\n---\n")
