a3bab388cab270d8e8b8384b9ab9355ced4cb2b3
Kalin Ivanov <kalin2578@gmail.com>
2023-12-30 21:16:54 +0200

    initial solution

---
+++ java/src/main/java/com/java/features/examples/advent_of_code/SnowCalibration.java
@@ -0,0 +1,54 @@
+package com.java.features.examples.advent_of_code;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
+
+@Slf4j
+public class SnowCalibration {
+  private static final Map<String, String> numberWordsToDigits = Map.of(
+          "one",   "one1one",
+          "two",   "two2two",
+          "three", "three3three",
+          "four",  "four4four",
+          "five",  "five5five",
+          "six",   "six6six",
+          "seven", "seven7seven",
+          "eight", "eight8eight",
+          "nine",  "nine9nine"
+  );
+
+  public static void main(String[] args) {
+    var fileName = "java/src/main/resources/input.txt";
+    var totalSum = 0;
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        totalSum += extractAndFormNumber(line);
+      }
+    } catch (IOException e) {
+      log.error("Error occurs while reading", e);
+    }
+    log.info("Total sum of calibration values: {}", totalSum);
+  }
+
+  private static int extractAndFormNumber(String line) {
+    var numbers = Arrays.stream(line.split("\\W+"))
+            .filter(word -> !word.isEmpty())
+            .map(word -> word.matches("\\d+") ? word : numberWordsToDigits.get(word.toLowerCase()))
+            .filter(Objects::nonNull)
+            .mapToInt(Integer::parseInt)
+            .toArray();
+
+    if (numbers.length < 2) {
+      return numbers.length == 1 ? numbers[0] : 0;
+    }
+    return numbers[0] + numbers[numbers.length - 1];
+  }
+}
