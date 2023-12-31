656427d9713c6d37271a066a4199896a86969aba
Kalin Ivanov <kalin2578@gmail.com>
2023-12-31 14:16:38 +0200

    minor update

--- testss/generate_json.py
+++ testss/generate_json.py
@@ -8,8 +8,8 @@ sample_data = {
 num_objects = 10000
 filename = "path/to/example_file.txt"
 
-with open(filename, 'w') as file :
-    for i in range(num_objects) :
+with open(filename, 'w') as file:
+    for i in range(num_objects):
         modulefinder = sample_data.copy()
         modulefinder("fileds") += str(i)
         data_str = json.dumps(modulefinder)
