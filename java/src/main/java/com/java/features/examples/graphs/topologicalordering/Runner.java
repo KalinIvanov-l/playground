b92259eed374f9c61a8a45c17d10af29fadc0e95
Kalin Ivanov <kalin2578@gmail.com>
2024-01-02 22:02:37 +0200

    initial

---
+++ java/src/main/java/com/java/features/examples/graphs/topologicalordering/Runner.java
@@ -0,0 +1,15 @@
+package com.java.features.examples.graphs.topologicalordering;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+  public static void main(String[] args) {
+    var topologicalOrdering = new TopologicalOrdering();
+    List<Vertex> graph = new ArrayList<>();
+
+    /**
+     * TODO: add logic here and use it.
+     */
+  }
+}
