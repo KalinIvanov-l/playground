cdedc9178d74a02e7e10ae7a8630cb5c032ea7ba
Kalin Ivanov <kalin2578@gmail.com>
2024-01-02 21:45:04 +0200

    add model

---
+++ java/src/main/java/com/java/features/examples/graphs/Vertex.java
@@ -0,0 +1,14 @@
+package com.java.features.examples.graphs;
+
+import java.util.List;
+
+public record Vertex(
+        String name,
+        boolean visited,
+        List<Vertex> neighbotList
+)
+{
+  public void addNeighbot(Vertex vertex) {
+    neighbotList.add(vertex);
+  }
+}
