99f4b2248a551a8218273890c116b737a4fdb80c
Kalin Ivanov <kalin2578@gmail.com>
2024-01-08 21:56:05 +0200

    update Vertex

--- java/src/main/java/com/java/features/examples/graphs/topologicalordering/shortestpath/Vertex.java
+++ java/src/main/java/com/java/features/examples/graphs/topologicalordering/shortestpath/Vertex.java
@@ -10,16 +10,23 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
-public class Vertex {
+public class Vertex extends Edge {
   private String name;
+  @Getter
   private boolean visited;
   private int minDistance;
   private Vertex predecessor;
   private List<Edge> adjancencyList;
+  private int weight;
 
-  public Vertex(String name) {
+  public Vertex(String name, Vertex predecessor, int weight) {
+    super(predecessor, weight);
     this.name = name;
     minDistance = Integer.MAX_VALUE;
     adjancencyList = new ArrayList<>();
+  }
+
+  public Vertex[] neighborliness() {
+    return new Vertex[0];
   }
 }
