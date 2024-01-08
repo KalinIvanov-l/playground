eecddb466f274dd50ffe75c5fabb8356be12beff
Kalin Ivanov <kalin2578@gmail.com>
2024-01-08 21:56:39 +0200

    initial

---
+++ java/src/main/java/com/java/features/examples/graphs/topologicalordering/shortestpath/ShortestPath.java
@@ -0,0 +1,29 @@
+package com.java.features.examples.graphs.topologicalordering.shortestpath;
+
+import com.java.features.examples.graphs.topologicalordering.TopologicalOrdering;
+
+import java.util.Deque;
+import java.util.List;
+
+public class ShortestPath {
+  private TopologicalOrdering topologicalOrdering;
+
+  public ShortestPath(List<Vertex> graph) {
+    graph.get(0).setMinDistance(0);
+  }
+
+  public void compute() {
+    Deque<Vertex> topologicalOrder = topologicalOrdering.getDeque();
+
+    while (!topologicalOrder.isEmpty()) {
+      Vertex vertex = topologicalOrder.pop();
+      for (Edge edge : vertex.neighborliness()) {
+        Vertex vertex1 = edge.getTarget();
+        if (vertex1.getMinDistance() < vertex.getMinDistance() + edge.getWeight()) {
+          vertex.setMinDistance(vertex1.getMinDistance() + edge.getWeight());
+          vertex1.setPredecessor(vertex);
+        }
+      }
+    }
+  }
+}
