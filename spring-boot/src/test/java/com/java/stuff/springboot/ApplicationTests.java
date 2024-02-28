f2fde4dd6a8a0a67cc74a24ccab643f22a4e417e
KalinIvanov-l <kalin2578@gmail.com>
2024-02-28 08:58:20 +0200

    Add assertion

--- spring-boot/src/test/java/com/java/stuff/springboot/ApplicationTests.java
+++ spring-boot/src/test/java/com/java/stuff/springboot/ApplicationTests.java
@@ -3,11 +3,15 @@ package com.java.stuff.springboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class ApplicationTests {
 
   @Test
   void contextLoads() {
+    var args = new String[] {};
+    Application.main(args);
+    assertThat(Application.class).isNotNull();
   }
-
 }
