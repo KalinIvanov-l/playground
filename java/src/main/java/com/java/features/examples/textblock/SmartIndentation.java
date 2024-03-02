5f3e4e89c59f565bbb448cff8935a78a79158bf6
KalinIvanov-l <kalin2578@gmail.com>
2024-03-02 17:55:58 +0200

    Tweak simple cleanup in the code

--- java/src/main/java/com/java/features/examples/textblock/SmartIndentation.java
+++ java/src/main/java/com/java/features/examples/textblock/SmartIndentation.java
@@ -6,16 +6,12 @@ import org.slf4j.LoggerFactory;
 public class SmartIndentation {
   private static final Logger LOG = LoggerFactory.getLogger(SmartIndentation.class);
   private static final String MESSAGE = """
-            It is great
-            when compilers care about conventions
-            Makes our life easier
-            """;
-
-  public static String smartIndentation() {
-    return MESSAGE;
-  }
+          It is great
+          when compilers care about conventions
+          Makes our life easier
+          """;
 
   public static void main(String[] args) {
-    LOG.info(smartIndentation());
+    LOG.info(MESSAGE);
   }
 }
