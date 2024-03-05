672f384b8f771dff932b9cdfd0f7db7c161daf65
KalinIvanov-l <kalin2578@gmail.com>
2024-03-05 08:44:09 +0200

    Add logger

--- SPARQL/src/main/java/com/sparql/test/Main.java
+++ SPARQL/src/main/java/com/sparql/test/Main.java
@@ -1,7 +1,11 @@
 package com.sparql.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+  private static final Logger LOG = LoggerFactory.getLogger(Main.class);
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    LOG.info("Hello world!");
   }
 }
