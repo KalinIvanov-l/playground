4f1070d8447d75b288fe73738318260cb7b17044
KalinIvanov-l <kalin2578@gmail.com>
2024-03-16 10:55:56 +0200

    Trying to resolve ParameterResolver issue in junit

---
+++ java/src/main/java/com/java/techstuff/examples/parameterresolver/Book.java
@@ -0,0 +1,11 @@
+package com.java.techstuff.examples.parameterresolver;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Book {
+  String title;
+  String author;
+}
