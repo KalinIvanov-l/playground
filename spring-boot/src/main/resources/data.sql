f03961625845588f33b42adbf73dd2d0acb4014b
Kalin Ivanov <kalin2578@gmail.com>
2024-05-28 17:24:16 +0300

    [example] Initial

---
+++ spring-boot/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO ADDRESS (street, city, state, zip) VALUES ('123 Main St', 'Springfield', 'IL', '62701');
+INSERT INTO ADDRESS (street, city, state, zip) VALUES ('456 Elm St', 'Shelbyville', 'IL', '62702');
---
+++ spring-boot/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE ADDRESS (
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         street VARCHAR(255),
+                         city VARCHAR(255),
+                         state VARCHAR(255),
+                         zip VARCHAR(20)
+);
