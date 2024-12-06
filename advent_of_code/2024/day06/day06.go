c6e1eec8daf2c83b2065927f61e6f39bceb6b29f
KalinIvanov-I <kalin2578@gmail.com>
2024-12-06 21:50:35 +0200

    aoC day06 - Guard Gallivant

--- advent_of_code/2024/day06/day06.go
+++ advent_of_code/2024/day06/day06.go
@@ -4,43 +4,49 @@ import (
 	"fmt"
 	"image"
 	"os"
-	"slices"
 	"strings"
 )
 
 func main() {
 	input, _ := os.ReadFile("advent_of_code/2024/day06/input.txt")
 
-	grid, start := map[image.Point]rune{}, image.Point{}
-	for y, s := range strings.Split(strings.TrimSpace(string(input)), "\n") {
-		for x, r := range s {
+	grid := make(map[image.Point]rune)
+	var start image.Point
+	for y, line := range strings.Split(strings.TrimSpace(string(input)), "\n") {
+		for x, r := range line {
+			p := image.Point{X: x, Y: y}
+			grid[p] = r
 			if r == '^' {
-				start = image.Point{X: x, Y: y}
+				start = p
 			}
-			grid[image.Point{X: x, Y: y}] = r
 		}
 	}
 	part1 := func(p image.Point, d int) int {
 		delta := []image.Point{{0, -1}, {1, 0}, {0, 1}, {-1, 0}}
-		seen := map[image.Point][]int{}
+		seen := make(map[image.Point][]int)
 		for {
 			if _, ok := grid[p]; !ok {
 				return len(seen)
 			}
-			if ds, ok := seen[p]; ok && slices.Contains(ds, d) {
-				return -1
+			if ds, ok := seen[p]; ok && len(ds) > 0 {
+				for _, dir := range ds {
+					if dir == d {
+						return -1
+					}
+				}
 			}
 			seen[p] = append(seen[p], d)
-			if n := p.Add(delta[d]); grid[n] == '#' {
-				d = (d + 1) % len(delta)
+			next := p.Add(delta[d])
+			if grid[next] == '#' {
+				d = (d + 1) % 4
 			} else {
-				p = n
+				p = next
 			}
 		}
 	}
 	part2 := 0
-	for p := range grid {
-		if grid[p] != '.' {
+	for p, r := range grid {
+		if r != '.' {
 			continue
 		}
 		grid[p] = '#'
@@ -49,6 +55,7 @@ func main() {
 		}
 		grid[p] = '.'
 	}
+
 	fmt.Println(part1(start, 0))
 	fmt.Println(part2)
 }
