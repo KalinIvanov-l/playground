1c46920d0c4e5e89dc60344e8c69b814fc12a1dd
KalinIvanov-I <kalin2578@gmail.com>
2024-12-10 21:36:40 +0200

    aoC day10 - Hoof It

---
+++ advent_of_code/2024/day10/day10.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"os"
+	"strings"
+)
+
+func main() {
+	input, _ := os.ReadFile("advent_of_code/2024/day10/input.txt")
+
+	grid := map[image.Point]rune{}
+	for y, s := range strings.Split(strings.TrimSpace(string(input)), "\n") {
+		for x, r := range s {
+			grid[image.Point{X: x, Y: y}] = r
+		}
+	}
+
+	part1, part2 := 0, 0
+	for p := range grid {
+		if grid[p] == '0' {
+			part1 += dfs(grid, p, map[image.Point]bool{})
+			part2 += dfs(grid, p, nil)
+		}
+	}
+	fmt.Println(part1)
+	fmt.Println(part2)
+}
+
+func dfs(grid map[image.Point]rune, p image.Point, seen map[image.Point]bool) (score int) {
+	if grid[p] == '9' {
+		if seen[p] {
+			return 0
+		} else if seen != nil {
+			seen[p] = true
+		}
+		return 1
+	}
+	for _, d := range []image.Point{{0, -1}, {1, 0}, {0, 1}, {-1, 0}} {
+		if n := p.Add(d); grid[n] == grid[p]+1 {
+			score += dfs(grid, n, seen)
+		}
+	}
+	return score
+}
